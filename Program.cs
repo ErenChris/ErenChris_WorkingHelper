cdaceed59f59f95a20f79e3271094397d9d6abd3
ErenChris <2485552681@qq.com>
2021-08-02 17:24:41 +0800

    Update

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using OpenQA.Selenium;
 using System;
 using WorkingHelper.Handler;
+using WorkingHelper.Models;
 
 namespace WorkingHelper
 {
@@ -8,19 +9,26 @@ namespace WorkingHelper
     {
         static void Main(string[] args)
         {
-            using (IWebDriver driver = new OpenQA.Selenium.Chrome.ChromeDriver())
-            {
-                driver.Navigate().GoToUrl("http://www.baidu.com");  //driver.Url = "http://www.baidu.com"是一样的
+            //using (IWebDriver driver = new OpenQA.Selenium.Chrome.ChromeDriver())
+            //{
+            //    driver.Navigate().GoToUrl("http://www.baidu.com");  //driver.Url = "http://www.baidu.com"是一样的
 
-                var source = driver.PageSource;
+            //    var source = driver.PageSource;
 
-                Console.WriteLine(source);
+            //    Console.WriteLine(source);
 
-                var byClassName = driver.FindElements(By.ClassName("text-color"));
-                Console.WriteLine(byClassName);
+            //    var byClassName = driver.FindElements(By.ClassName("text-color"));
+            //    Console.WriteLine(byClassName);
 
-                Console.ReadLine();
-            }
+            //    Console.ReadLine();
+            //}
+            string result = "";
+            InformationForWebBrowser informationForWebBrowser = new InformationForWebBrowser("https://www.baidu.com");
+            WebHandler webHandler = new WebHandler();
+            result = webHandler.GetHtmlStr(informationForWebBrowser.Url, "UTF-8");
+
+            Console.WriteLine(result);
+            Console.ReadLine();
         }
     }
 }
