664b0d5123d243e3129a42e519db4d42d9ef7892
钰涛 徐 <2485552681@qq.com>
2021-11-21 20:22:16 +0800

    Update

--- 3DPlotDataMaker/Program.cs
+++ 3DPlotDataMaker/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
 
 namespace _3DPlotDataMaker
 {
@@ -10,6 +12,50 @@ namespace _3DPlotDataMaker
     {
         static void Main(string[] args)
         {
+            string inputFilePath = @".\Input.txt";
+            string outputFilePath = @".\Output.txt";
+            string line = null;
+            string[] temp = null;
+            string result = null;
+
+            try
+            {
+                using (StreamReader sr = new StreamReader(inputFilePath))
+                {
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        temp = Regex.Split(line, ": ");
+                        if (temp[1].Contains("["))
+                        {
+                            result = result + "\n" + temp[1];
+                        }
+                        else
+                        {
+                            result += temp[1];
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                using(StreamWriter sw = new StreamWriter(outputFilePath))
+                {
+                    sw.Write(result);
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw;
+            }
+
+            Console.WriteLine("Done!");
+            Console.ReadLine();
         }
     }
 }
